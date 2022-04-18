a69d331c4216b124a65f9d7d9afd812074977335
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-04-18 20:31:46 -0300

    feat: home page implemented

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import {engine} from 'express-handlebars';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.routes.js'
 dotenv.config()
@@ -10,21 +9,16 @@ dotenv.config()
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.engine('handlebars', engine({
-    extname: 'hbs',
-    defaultLayout: false,
-    layoutsDir: '/views/layout'
-}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('env', process.env.NODE_ENV);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', authRouter)
+app.use('/', authRouter)
 
 app.get('/', (req, res, next) => {
     res.render('index');
