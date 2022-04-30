94de1d2950bfc0573ab0d1c13c76293cb261b93a
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-04-30 10:19:30 -0300

    feat: testing if the user is allowed to use an existing email

--- tests/integration/authentication.test.js
+++ tests/integration/authentication.test.js
@@ -2,16 +2,13 @@ const database =  require('../../src/database/connection.js');
 const bcrypt = require('bcrypt');
 const request = require('supertest')
 const app = require('../../src/app')
-// const { saveUser } = require('../../src/app/controllers/auth.controller')
 
 beforeAll(async () => {await database.memoryDbConnect()})
 // beforeEach(async () => {await database.clearMemoryDb()})
 afterAll(async () => {await database.closeMemoryDb()})
 
 describe('Authentication', () => {
-
-
-    it('should save user with valid credentials', (done) => {
+    it('should save a user with valid credentials', (done) => {
         const mockUser = {
             name: "eduardo",
             email: "dudu@gmail.com",
@@ -31,10 +28,28 @@ describe('Authentication', () => {
         } catch(error) {
             expect(error).toMatch('error')
         }
+    })
 
+    it('should return an error when saving user with existing email', (done) => {
+        const mockUser = {
+            name: "eduardo",
+            email: "dudu@gmail.com",
+            password: "eduardo",
+            confirmPassword: "eduardo"
+        }
 
-        
-            
+        try {
+            request(app)
+            .post('/auth/register')
+            .send(mockUser)
+            .expect(400)
+            .end(async (error, response) => {
+                if(error) return done(error)
+                return done()
+            })
+        } catch(error) {
+
+        }
     })
 
 });
