ba0e6819300a5c4dac64ae602884abdc64f3f5dd
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-05-03 20:41:14 -0300

    feat(home): home page rendering popular and top rated shows

--- src/app/controllers/home.controller.js
+++ src/app/controllers/home.controller.js
@@ -1,14 +1,21 @@
 const Movie = require('../../services/tmdb/movies')
+const Show =  require('../../services/tmdb/shows')
 const posterPathUrl = 'https://image.tmdb.org/t/p/original/'
 
 async function getHomePage(req, res, next) {
     const movies = new Movie()
+    const shows = new Show()
     const popularMovies = await movies.getPopularMovies()
     const topRatedMovies = await movies.getTopRatingMovies()
+    const popularShows = await shows.getPopularShows()
+    const topRatedShows = await shows.getTopRatedShows()
+
     res.render('home', {
         posterPathUrl,
         popularMovies: popularMovies.results,
-        topRatedMovies: topRatedMovies.results
+        topRatedMovies: topRatedMovies.results,
+        popularShows: popularShows.results,
+        topRatedShows: topRatedShows.results
     })
 }
 
