aff8b0489266413be07e09a796f4e939c8802ca8
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-05-06 20:13:23 -0300

    feat(search, details): when searching a movie the app add a type field in each object, specifying if that show is a movie or a tv show, details are now part of the search so the app can tell if it must retrieve the details of a movie or a tv show

--- src/app/controllers/search.controller.js
+++ src/app/controllers/search.controller.js
@@ -1,4 +1,6 @@
 const Search = require('../../services/tmdb/search')
+const Movie = require('../../services/tmdb/movies')
+const Show = require('../../services/tmdb/shows')
 const posterPathUrl = 'https://image.tmdb.org/t/p/original/';
 
 class searchController {
@@ -7,10 +9,29 @@ class searchController {
             const {query} = req.query
             const showsResult = await Search.searchShow(query);
             const moviesResult = await Search.searchMovie(query);
+
+            // Aqui tut a adicionando um tipo nos filmes e series pra poder peagar os detalhes na hora da busca
+            // por enquando tá tudo certo mas faz mais uns testes ai pra ter certeza
+
+            for(let i = 0; i < showsResult.results.length; i++) {
+                Object.defineProperty(showsResult.results[i], 'type', {
+                    value: 'show'
+                })
+            }
+
+            for(let i = 0; i < moviesResult.results.length; i++) {
+                Object.defineProperty(moviesResult.results[i], 'type', {
+                    value: 'movie'
+                })
+            }
+
+            
             const result = showsResult.results.concat(moviesResult.results)
             const resultSorted = result.sort((a, b) => {
                 if(a.popularity > b.popularity) return -1
             })
+
+            console.log(resultSorted[0].type)
 
             res.render('searchResult', {
                 posterPathUrl,
@@ -19,6 +40,26 @@ class searchController {
         } catch(error) {
             res.send(error)
         }
+    }
+    async getDetails(req, res, next) {
+        const { id } = req.params
+        const { type } = req.query
+
+        if(type == 'show') {
+            const showDetails = await Show.getShowDetails(id)
+            return res.render('details', {
+                posterPathUrl,
+                details: showDetails
+            })
+        } else {
+            // type == movie
+            const movieDetails = await Movie.getMovieDetails(id)
+            return res.render('details', {
+                posterPathUrl,
+                details: movieDetails
+            })
+        }
+
     }
 }
 
--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -1,9 +1,9 @@
-const { searchResult } = require('../app/controllers/search.controller.js');
+const { searchResult, getDetails } = require('../app/controllers/search.controller.js');
 const { isUserAuthorized } = require('../app/middlewares/permissions')
 const express = require('express')
 const searchRouter = express.Router()
 
 searchRouter.get('/', isUserAuthorized, searchResult)
-// searchRouter.get('/details', isUserAuthorized, getShowDetails) // Todo add show id to the route
+searchRouter.get('/details/:id', getDetails)
 
 module.exports = searchRouter
